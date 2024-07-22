6f4a5a0a1b8b3334e52abf2b34db80df54f2dbe9
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-22 11:18:35 +0300

    Modified models.py

--- server/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
-
-db = SQLAlchemy()
-
-class Plant(db.Model, SerializerMixin):
-    __tablename__ = 'plants'
-
-    id = db.Column(db.Integer, primary_key=True)
